f3f7ca935b05fbe8b197bcb200ea739f314c1a37
MartinDev <87422391+Louraes202@users.noreply.github.com>
2022-10-25 12:47:20 +0100

    Add files via upload

---
+++ Computer Systems Programming/Exercises/MOD09_Exer3/MOD09_Exer3/Bilhete.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOD09_Exer3
+{
+    public class Bilhete
+    {
+        double valor;
+
+        public Bilhete()
+        {
+
+        }
+
+        public Bilhete(double nvalor)
+        {
+            this.valor = nvalor;
+        }
+
+        public double imprimirValor()
+        {
+            return valor;
+        }
+    }
+}
