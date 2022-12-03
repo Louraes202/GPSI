38745e82abca98b1643c66b5f848968fe9807013
MartinDev <87422391+Louraes202@users.noreply.github.com>
2022-12-03 17:36:54 +0000

    Add files via upload

---
+++ Computer Systems Programming/C#/MOD09/MOD09_Exer5/MOD09_Exer5/Produto.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOD09_Exer5
+{
+    class Produto
+    {
+        string nome;
+        double precoCusto;
+        double precoVenda;
+        double margemLucro;
+
+        public Produto()
+        {
+            nome = "";
+            precoCusto = 0;
+            precoVenda = 0;
+            margemLucro = 0;
+        }
+
+        public Produto(string a, double b, double c)
+        {
+            this.nome = a;
+            this.precoCusto = b;
+            this.precoVenda = c;
+        }
+
+        public void change_precoCusto(double a)
+        {
+            this.precoCusto = a;
+        }
+
+        public void change_precoVenda(double a)
+        {
+            this.precoVenda = a;
+        }
+
+
+        public double getPrecoCusto()
+        {
+            return this.precoCusto;
+        }
+
+        public double getPrecoVenda()
+        {
+            return this.precoVenda;
+        }
+        public double calcularMargemLucro()
+        {
+            this.margemLucro = this.precoVenda - this.precoCusto;
+            return margemLucro;
+        }
+
+        public double getMargemLucroPercentagem()
+        {
+            double permargemLucro = (margemLucro * 100) / precoCusto;
+            return permargemLucro;
+        }
+    }
+}
---
+++ Computer Systems Programming/C#/MOD09/MOD09_Exer5/MOD09_Exer5/Program.cs
@@ -0,0 +1,14 @@
+﻿using MOD09_Exer5;
+
+Produto p1 = new Produto();
+
+Console.WriteLine("Digite o preço do custo: ");
+p1.change_precoCusto(Double.Parse(Console.ReadLine()));
+Console.WriteLine("Digite o preço do venda: ");
+p1.change_precoVenda(Double.Parse(Console.ReadLine()));
+
+
+Console.WriteLine("Preço de custo: {0}", p1.getPrecoCusto());
+Console.WriteLine("Preço de custo: {0}", p1.getPrecoVenda());
+Console.WriteLine("Margem de lucro: {0}", p1.calcularMargemLucro());
+Console.WriteLine("Margem de lucro em percentagem: {0}%", p1.getMargemLucroPercentagem());
