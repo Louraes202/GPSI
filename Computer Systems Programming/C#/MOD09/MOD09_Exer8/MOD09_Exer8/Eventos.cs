d27320c126d1c4cd880d7424fb0f716ac535b002
MartinDev <87422391+Louraes202@users.noreply.github.com>
2022-12-03 17:37:53 +0000

    Add files via upload

---
+++ Computer Systems Programming/C#/MOD09/MOD09_Exer8/MOD09_Exer8/Eventos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOD09_Exer8
+{
+    class Eventos
+    {
+        string codigo;
+        string descricao;
+        string local;
+        string data;
+        double custo_de_organizacao;
+        double valor_de_entrada;
+        
+    }
+}
