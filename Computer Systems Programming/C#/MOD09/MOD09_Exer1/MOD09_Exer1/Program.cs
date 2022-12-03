14e2af2ba2480e1a8d6fdcc865df4b6f25b00f8a
MartinDev <87422391+Louraes202@users.noreply.github.com>
2022-12-03 17:28:25 +0000

    Add files via upload

---
+++ Computer Systems Programming/C#/MOD09/MOD09_Exer1/MOD09_Exer1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOD09_Exer1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double lado;
+            Console.WriteLine("Lado do quadrado: ");
+            bool doublelado = double.TryParse(Console.ReadLine(), out lado);
+            while (doublelado == false)
+            {
+                Console.WriteLine("O lado tem de ser um valor real!");
+                Console.WriteLine("Lado do quadrado: ");
+                doublelado = double.TryParse(Console.ReadLine(), out lado);
+            }
+
+            if (lado >= 0 && lado <= 20)
+            {
+                Quadrado quadrado1 = new Quadrado(lado);
+                Console.WriteLine("Area do Quadrado: " + quadrado1.area());
+                Console.WriteLine("Diagonal do Quadrado: " + quadrado1.diagonal());
+                Console.WriteLine("Perímetro do Quadrado: " + quadrado1.perimetro());
+                Console.ReadLine();
+            }
+            else if (lado < 0 || lado > 20) {
+                while (lado < 0 || lado > 20)
+                {
+                    Console.WriteLine("O valor do lado tem de ser um valor real entre entre 0 e 20.");
+                    Console.WriteLine("Lado do quadrado: ");
+                    doublelado = double.TryParse(Console.ReadLine(), out lado);
+                    while (doublelado == false)
+                    {
+                        Console.WriteLine("O lado tem de ser um valor real!");
+                        Console.WriteLine("Lado do quadrado: ");
+                        doublelado = double.TryParse(Console.ReadLine(), out lado);
+                    }
+                }
+                Quadrado quadrado1 = new Quadrado(lado);
+                Console.WriteLine("Area do Quadrado: " + quadrado1.area());
+                Console.WriteLine("Diagonal do Quadrado: " + quadrado1.diagonal());
+                Console.WriteLine("Perímetro do Quadrado: " + quadrado1.perimetro());
+                Console.ReadLine();
+            }
+            
+                                 
+        }
+    }
+}
---
+++ Computer Systems Programming/C#/MOD09/MOD09_Exer1/MOD09_Exer1/Quadrado.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOD09_Exer1
+{
+    internal class Quadrado
+    {
+        private double lado;
+        public Quadrado() // Construtor vazio
+        {
+            this.lado = 1.0;
+        }
+
+        public Quadrado(double lado) // Construtor com parâmetros
+        {
+            this.lado = lado;
+        }
+
+        public double area() // Area
+        {
+            double area = 2 * lado;
+            return area;
+        }
+
+        public double diagonal() // Diagonal
+        {
+            double diagonal = Math.Sqrt(2 * lado);
+            return diagonal;
+        }
+
+        public double perimetro() // Perimetro
+        {
+            double perimetro = 4 * lado;
+            return perimetro;
+        }
+    }
+}
